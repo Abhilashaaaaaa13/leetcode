0bb20824a3d8b4b5fc40414082f0670b616a6872
Abhilashaaaaaa13 <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2025-08-11 08:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (41.16%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-vector<int> digitFreq(int n) {
-        vector<int> freq(10, 0);
-        while (n > 0) {
-            freq[n % 10]++;
-            n /= 10;
-        }
-        return freq;
-    }
-
-    // Compare two frequency arrays (size 10) for equality
-    bool equalFreq(const vector<int>& a, const vector<int>& b) {
-        for (int i = 0; i < 10; ++i)
-            if (a[i] != b[i]) return false;
-        return true;
-    }
+string reorder(int n){
+    string s = to_string( n);
+    sort(s.begin(),s.end());
+    return s;
+}
     bool reorderedPowerOf2(int n) {
-          vector<int> target = digitFreq(n);
-
-        // Check all powers of two up to 2^30 (fits in 32-bit signed int ie till limit ie 10^9)
-        for (int i = 0; i <= 30; ++i) {
-            int powerof2 = (int)pow(2, i);; // 2^i
-            if (equalFreq(target, digitFreq(powerof2)))
+        string s = reorder(n);
+        for(int p=0;p<=29;p++){
+            if(s==reorder(1 << p)){
                 return true;
+            }
         }
         return false;
-
     }
 };
