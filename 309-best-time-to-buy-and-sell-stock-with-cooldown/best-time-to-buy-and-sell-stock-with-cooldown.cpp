5916e3656c4c88405c2736d75620998a84495fc7
Abhilashaaaaaa13 <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2025-08-14 21:55:53 +0530

    Time: 4 ms (20.56%) | Memory: 16 MB (64.09%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-int solve(vector<int>& prices,int index,int buy,vector<vector<int>>&dp){
-    if(index>=prices.size()){
-        return 0;
-    }
-    if(dp[index][buy]!= -1){
-        return dp[index][buy];
-    }
+int solve(vector<int>& prices){
+    
+    int n = prices.size();
+    vector<vector<int>>dp(n+2,vector<int>(2,0));
     int profit =0;
+    for(int index = n-1; index >=0 ;index--){
+        for(int buy = 0 ; buy<=1;buy++){
+    
     if(buy){
-     int buykaro = -prices[index]+solve(prices,index+1,0,dp);
-     int skipkaro = 0+solve(prices,index+1,1,dp);
+     int buykaro = -prices[index]+dp[index+1][0];
+     int skipkaro = 0+dp[index+1][1];
      profit = max(buykaro,skipkaro);
     }
     else{
-        int sellkaro = prices[index]+solve(prices,index+2,1,dp);
-        int skipkaro = 0+solve(prices,index+1,0,dp);
+        int sellkaro = prices[index]+dp[index+2][1];
+        int skipkaro = 0+dp[index+1][0];
         profit = max(skipkaro,sellkaro);
     }
-    return dp[index][buy] = profit;
+    dp[index][buy]=profit;
+    }}
+    return dp[0][1];
 }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return solve(prices,0,1,dp);
+        
+        return solve(prices);
     }
 };
