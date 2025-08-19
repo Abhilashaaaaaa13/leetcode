f4f84bbfec5b8a8d17e9d0d4334980a09cd109a8
Abhilashaaaaaa13 <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2025-08-19 12:48:09 +0530

    Time: 79 ms (52.39%) | Memory: 14.2 MB (73.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-int solve(vector<int>& nums,int index,int n , int p,vector<vector<int>>&dp){
-    if(index>=n){
-        return 0;
-    }
-  int take =0;
-  if(p != -1 && dp[index][p] != -1){
-    return dp[index][p];
-  }
-    if(p==-1 || nums[index]>nums[p]){
-     take = 1+solve(nums,index+1,n,index,dp);
-    }
-    int skip = solve(nums,index+1,n,p,dp);
-    if(p!= -1)
-  dp[index][p] = max(take,skip);
-  return max(take,skip) ;
-}
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(nums,0,n,-1,dp);
+        vector<int>t(n,1);
+        int maxlis=1;
+        for(int i=0;i<n;i++){
+        for(int j =0;j<i;j++){
+          if(nums[j]<nums[i]){
+            t[i]= max(t[i],t[j]+1);
+            maxlis = max(maxlis,t[i]);
+          }
+            }
+        }
+        return maxlis;
     }
 };
