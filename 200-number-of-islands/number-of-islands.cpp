1e404973a48c3098a2e2dd3c92c264de39c110dd
Abhilashaaaaaa13 <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2025-09-08 11:57:44 +0530

    Time: 92 ms (5.99%) | Memory: 36.6 MB (5.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-  void dfs(vector<vector<char>>& grid,int i , int j){
-      int m = grid.size();
-      int n = grid[0].size();
-      if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0')
-        return ;
-      
-      grid[i][j]='0';
-      dfs(grid,i+1,j);
-      dfs(grid,i,j+1);
-      dfs(grid,i,j-1);
-      dfs(grid,i-1,j);
-      
-  }
+vector<vector<int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
+
+void bfs(vector<vector<char>>& grid, int i , int j){
+int m = grid.size();
+int n = grid[0].size();
+queue<pair<int,int>>q;
+q.push({i,j});
+grid[i][j]='0';
+while(!q.empty()){
+    auto [x,y] = q.front();
+    q.pop();
+    for(vector<int>dir : directions){
+        int newx = x + dir[0];
+        int newy = y+ dir[1];
+        if(newx >= 0 &&  newy >= 0 && newx < m && newy < n && grid[newx][newy]=='1'){
+            grid[newx][newy]='0';
+            q.push({newx, newy});
+        }
+    }
+}
+}
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int count=0 ;
-        for(int i =0; i<m ;i++){
-            for(int j =0 ; j<n ;j++){
+        int count =0;
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
                 if(grid[i][j]=='1'){
-                     count++;
-               dfs(grid,i,j);
-              
-
+                    count++;
+                    bfs(grid,i,j);
                 }
             }
         }
