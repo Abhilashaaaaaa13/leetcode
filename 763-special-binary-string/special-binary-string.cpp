14f090ca933579e860f54e0e1318a52b214ea186
ABHILASHA <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2026-02-20 12:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (80.52%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string>blocks;
+        int count = 0;
+        int start = 0;
+        for(int i =0;i<s.length();i++){
+            if(s[i]=='1'){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count ==0){
+                string inner = s.substr(start+1,i-start-1);
+                blocks.push_back("1"+makeLargestSpecial(inner)+"0");
+                start = i+1;
+            }
+        }
+        sort(blocks.begin(),blocks.end(),greater<string>());
+        string result = "";
+        for(string b:blocks){
+            result += b;
+        }
+        return result;
+    }
+};
