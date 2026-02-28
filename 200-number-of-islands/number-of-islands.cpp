d9a34dc0173dcbc72660d3225f73e623cf90b204
ABHILASHA <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2026-02-28 13:21:15 +0530

    Time: 77 ms (6.26%) | Memory: 36.7 MB (5.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-vector<vector<int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
 
-void bfs(vector<vector<char>>& grid, int i , int j){
-int m = grid.size();
+vector<vector<int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
+void bfs(vector<vector<char>>& grid,int i ,int j){
+    int m = grid.size();
 int n = grid[0].size();
-queue<pair<int,int>>q;
-q.push({i,j});
-grid[i][j]='0';
-while(!q.empty()){
-    auto [x,y] = q.front();
-    q.pop();
-    for(vector<int>dir : directions){
-        int newx = x + dir[0];
-        int newy = y+ dir[1];
-        if(newx >= 0 &&  newy >= 0 && newx < m && newy < n && grid[newx][newy]=='1'){
-            grid[newx][newy]='0';
-            q.push({newx, newy});
+    queue<pair<int,int>>q;
+    q.push({i,j});
+    grid[i][j]='0';
+    while(!q.empty()){
+        auto[x,y]=q.front();
+        q.pop();
+        for(vector<int>dir:directions){
+            int newx = x+dir[0];
+            int newy = y+dir[1];
+            if(newx>=0 && newy>=0 && newx<m && newy<n && grid[newx][newy]=='1'){
+                grid[newx][newy]='0';
+                q.push({newx,newy});
+            }
         }
     }
 }
-}
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+    int numIslands(vector<vector<char>>& grid) {int m = grid.size();
+int n = grid[0].size();
         int count =0;
         for(int i=0;i<m;i++){
-            for(int j =0;j<n;j++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    count++;
                     bfs(grid,i,j);
+                    count++;
                 }
             }
         }
