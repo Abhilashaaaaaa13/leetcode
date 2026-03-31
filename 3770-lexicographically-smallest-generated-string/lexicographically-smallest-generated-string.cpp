dec1ac7bb5cbeaf4aa9be516cefd4f1c45288084
ABHILASHA <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2026-03-31 18:11:11 +0530

    Time: 70 ms (10.17%) | Memory: 10.8 MB (89.83%) - LeetSync

---
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string generateString(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        int sz = n + m - 1;
+
+        string word(sz, '$');
+
+        // Step 1: Apply 'T' constraints
+        for (int i = 0; i < n; i++) {
+            if (str1[i] == 'T') {
+                for (int j = 0; j < m; j++) {
+                    if (word[i + j] == '$') {
+                        word[i + j] = str2[j];
+                    }
+                    else if (word[i + j] != str2[j]) {
+                        return "";
+                    }
+                }
+            }
+        }
+
+        // Step 2: Copy original state
+        string word2 = word;
+
+        // Step 3: Process 'F' constraints
+        for (int i = 0; i < n; i++) {
+            if (str1[i] == 'F') {
+
+                int cntBlank = 0, cntSame = 0;
+                int lastBlank = -1;
+
+                for (int j = 0; j < m; j++) {
+
+                    if (word2[i + j] == '$') {
+                        if (word[i + j] == '$') {
+                            word[i + j] = 'a';
+                        }
+                        lastBlank = i + j;
+                        cntBlank++;
+                    }
+
+                    if (word[i + j] == str2[j]) {
+                        cntSame++;
+                    }
+                }
+
+                if ((cntSame == m) && (cntBlank == 0)) return "";
+
+                if ((cntSame == m) && (cntBlank > 0)) {
+                    word[lastBlank] = 'b';
+                }
+            }
+        }
+
+        return word;
+    }
+};
