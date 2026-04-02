b8a0d635179f38effbc30302a6059f7fc36eafe2
ABHILASHA <164385955+Abhilashaaaaaa13@users.noreply.github.com>
2026-04-02 13:05:17 +0530

    Time: 668 ms (25.11%) | Memory: 276 MB (37.87%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ int m ;
+int n;
+vector<vector<vector<int>>>dp;
+int solve(vector<vector<int>>& coins, int i , int j,int neu){
+    if(i==m-1 && j ==n-1){
+        if(coins[i][j]<0 && neu>0){
+            return 0;
+        }
+        return coins[i][j];
+    }
+    if(i>=m || j>=n){
+        return INT_MIN;
+    }
+    if(dp[i][j][neu]!=INT_MIN)return dp[i][j][neu];
+    int take = coins[i][j]+max(solve(coins,i+1,j,neu),solve(coins,i,j+1,neu));
+    int skip = INT_MIN;
+    if(coins[i][j]<0 && neu>0){
+        int skipright = solve(coins,i,j+1,neu-1);
+        int skipdown = solve(coins,i+1,j,neu-1);
+        skip = max(skipright,skipdown);
+    }
+    return dp[i][j][neu]= max(take,skip);
+
+
+}
+    int maximumAmount(vector<vector<int>>& coins) {
+        m = coins.size();
+        n = coins[0].size();
+       dp.resize(m,vector<vector<int>>(n,vector<int>(3,INT_MIN)));
+        return solve(coins,0,0,2);
+    }
+};
